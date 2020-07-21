170c5cc164e7f00093a9e7bb8f696fdc04bae913
Myhyun <mattyihyun@gmail.com>
2020-07-20 19:09:28 -0700

    added db folder

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Western Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Jalapeno Burger', FALSE);
